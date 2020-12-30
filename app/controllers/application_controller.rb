11edcff92e051584666b9bbcccec4155194d1991
Ayako Amano <ayakoamano21@gmail.com>
2020-12-31 02:57:55 +0900

    debuged now home route works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, :authenticate_user!, if: :devise_controller?
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only [:create, :index, :destroy]
+  resources :items, only: [:create, :index, :patch, :destroy]
+  resources :lists, only: [:create, :index, :patch, :destroy]
 end
