3290995c2133092e1a464d2f379ee6654086c5de
Ayako Amano <ayakoamano21@gmail.com>
2021-01-01 12:54:55 +0900

    deguged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, :authenticate_user!, if: :devise_controller?
 
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 end
